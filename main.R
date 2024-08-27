07d37f361f5457270573e3577826028ca528b02e
DubielaODestruidor <joaopedro.dubiela@hotmail.com>
2024-08-27 15:43:13 -0300

    Gráficos para comparação feitos

--- main.R
+++ main.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 # Loading data
 data_red <- read.csv("./bases-de-dados/winequality-red.csv", sep = ";")
 data_white <- read.csv("./bases-de-dados/winequality-white.csv", sep = ";")
@@ -11,18 +13,42 @@ if (all.equal(colnames(data_red), colnames(data_white))) {
   print("The databases have different columns")
 }
 
-# Checking wine quality of both data.frames
-print("Red wine quality ->")
+# Checking wine quality count of both data.frames
 red_quality_quantity <- table(data_red$quality)
 red_quality <- data_red$quality
-print(red_quality_quantity)
-print(red_quality[1:10])
 
-print("White wine quality ->")
 white_quality_quantity <- table(data_white$quality)
 white_quality <- data_white$quality
-print(white_quality_quantity)
-print(white_quality[1:10])
 
-# Setting seed for reproducibility of the following analysis
-set.seed(123)
+# Dataviz of wine quality count
+red_plot_quality_quantity <- ggplot(data_red, aes(x = quality)) +
+  geom_bar(fill = "red") +
+  labs(title = "Red Wine Quality", x = "Quality (Class)", y = "Quantity (Count)") +
+  geom_text(stat = "count", aes(label = ..count..), vjust = -0.5) +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "grey"),
+  ) +
+  scale_x_continuous(
+    breaks = seq(
+      min(red_quality),
+      max(red_quality),
+      by = 1)
+    )
+
+white_plot_quality_quantity <- ggplot(
+  data_white, aes(x = quality)) +
+  geom_bar(fill = "white") +
+  labs(title = "White Wine Quality", x = "Quality (Class)", y = "Quantity (Count)") +
+  geom_text(stat = "count", aes(label = ..count..), vjust = -0.5) +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "grey"),
+  ) +
+  scale_x_continuous(
+    breaks = seq(
+      min(white_quality),
+      max(white_quality),
+      by = 1
+    )
+  )
