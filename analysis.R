eaf92b2882ed9df5730581b4a6124cfabd5edafd
DubielaODestruidor <joaopedro.dubiela@hotmail.com>
2024-09-07 19:43:21 -0300

    TODO: Finalize the readme to explain the code!

--- analysis.R
+++ analysis.R
@@ -1,5 +1,44 @@
-# Plotting ROC curve
-# ref: https://www.r-bloggers.com/2016/08/plotting-roc-curves/
-library(pROC)
-roc_red <- roc(data_red$quality, predict(rf_red, data_red))
-roc_white <- roc(data_white$quality, predict(rf_white, data_white))
+library(dplyr)
+library(randomForest)
+library(caret)
+set.seed(123)
+
+# Loading the models
+# NOTE: Models cannot be attributed to variables in R
+load("./models/rf_red.RData")
+load("./models/rf_white.RData")
+
+# Showing the models
+print(rf_red)
+print(rf_white)
+
+# Visualizing the importance of each characteristic
+varImpPlot(rf_red)
+varImpPlot(rf_white)
+
+# Making predictions
+# predict ref: https://www.rdocumentation.org/packages/randomForest/versions/4.7-1.1/topics/predict.randomForest
+data_red <- read.csv("./databases/winequality-red-balanced-norm.csv",
+                     sep = ",")
+data_red <- data_red %>%
+  mutate(quality = as.factor(quality))
+predictions_red <- predict(rf_red, data_red) %>%
+  trunc() %>% # Rounding to keep the factor levels equal data_red$quality
+  as.factor()
+
+data_white <- read.csv("./databases/winequality-white-balanced-norm.csv",
+                       sep = ",")
+data_white <- data_white %>%
+  mutate(quality = as.factor(quality))
+predictions_white <- predict(rf_white, data_white) %>%
+  trunc() %>%
+  as.factor()
+
+# Confusion matrix
+cat("Red wine confusion matrix\n")
+print(confusionMatrix(predictions_red, data_red$quality))
+cat("---------------------------------------------\n")
+
+cat("White wine confusion matrix\n")
+print(confusionMatrix(predictions_white, data_white$quality))
+cat("---------------------------------------------\n")
--- random_forest.R
+++ random_forest.R
@@ -8,25 +8,17 @@ data_white <- read.csv("./databases/winequality-white-balanced-norm.csv",
                        sep = ",")
 
 # Random Forest training
-# ref: https://www.listendata.com/2014/11/random-forest-with-r.html#decision-tree-vs-random-forest
+# ref: https://www.rdocumentation.org/packages/randomForest/versions/4.7-1.1/topics/randomForest
 red_training_time <- system.time({
   rf_red <- randomForest(quality ~ ., data = data_red, ntree = 1000)
 })
-cat("--------rf_red--------")
-print(rf_red)
 cat("Red training time: ", red_training_time["elapsed"], "seconds",  "\n")
-cat("---------------------- \n")
 
 white_training_time <- system.time({
   rf_white <- randomForest(quality ~ ., data = data_white, ntree = 1000)
 })
-cat("-------rf_white-------")
-print(rf_white)
 cat("White training time: ", white_training_time["elapsed"], "seconds",  "\n")
-cat("---------------------- \n")
 
-varImpPlot(rf_red)
-varImpPlot(rf_white)
 
 # Saving the models
 save(rf_red, file = "./models/rf_red.RData")
